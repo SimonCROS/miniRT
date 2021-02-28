f7346153f384d7fb861f8b9d4d8742d9a81285aa
Simon Cros <simon.dc.cros@gmail.com>
2021-02-28 20:50:15 +0100

    Save

--- includes/entity/camera.h
+++ includes/entity/camera.h
@@ -24,6 +24,7 @@ typedef struct	s_camera
 	float		fov;
 }				t_camera;
 
-t_camera		*new_camera(t_vector3 position, t_vector3 direction, float fov);
+t_camera	*parse_camera(t_list *data);
+t_camera	*new_camera(t_vector3 position, t_vector3 direction, float fov);
 
 #endif
--- src/entity/camera.c
+++ src/entity/camera.c
@@ -12,6 +12,41 @@
 
 #include "minirt.h"
 
+static int	rot_deserialize(const char *str, t_vector3 *vector)
+{
+	t_vector3	rot;
+
+	if (!vec3_deserialize(str, &rot))
+		return (0);
+	if (rot.x < -1 || rot.x > 1 ||
+		rot.y < -1 || rot.y > 1 ||
+		rot.z < -1 || rot.z > 1)
+		return (0);
+	*vector = rot;
+	return (1);
+}
+
+t_camera	*parse_camera(t_list *data)
+{
+	t_vector3	pos;
+	t_vector3	rot;
+	float		width;
+	t_color		color;
+	int			e;
+
+	if (data->size != 5)
+		return (NULL);
+	e = 1;
+	e = e && vec3_deserialize((char *)lst_get(data, 1), &pos);
+	e = e && rot_deserialize((char *)lst_get(data, 2), &rot);
+	e = e && is_int((char *)lst_get(data, 3));
+	e = e && color_deserialize((char *)lst_get(data, 4), &color);
+	if (!e)
+		return (NULL);
+	width = fabsf(ft_atof((char *)lst_get(data, 3)));
+	return (new_square(width, pos, rot, color));
+}
+
 t_matrix44	look_at(t_vector3 from, t_vector3 to)
 {
 	static t_vector3	tmp = (t_vector3) { 0, 1, 0 };
