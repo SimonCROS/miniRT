a949d4b7af64cbc5e13ffb14bd8478588af84736
Simon Cros <scros@student.42lyon.fr>
2021-03-16 16:02:34 +0100

    Save

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 cd $(dirname "$0")/..
+RENDER="render.bmp"
 TIMEFORMAT="Time : %Rs"
 mkdir -p benchmark/renders
+rm -f $RENDER
 for entry in "scenes"/*.rt "scenes"/crash/*.rt
 do
 	printf "\033[1;94mTesting %s...\033[0m\n" $entry
-	time ./miniRT $entry --save
-	if [ $? -eq 0 ]
+	time leaks -quiet -atExit -- ./miniRT $entry --save
+	if [ -f "$RENDER" ]
 	then
-		mv -f render.bmp benchmark/renders/$(basename ${entry%.*}).bmp
+		mv -f $RENDER benchmark/renders/$(basename ${entry%.*}).bmp
 	fi
 	printf "\n"
 done
--- src/display/window.c
+++ src/display/window.c
@@ -30,14 +30,21 @@ void	init_window(char *file, t_scene *scene)
 	vars.mlx = mlx_init();
 	if (!vars.mlx)
 	{
-		printf("Error, can't generate the frame\n");
-		exit(1);  // TODO
+		free_scene(scene);
+		perror("Error\nCan't generate the frame");
+		exit(EXIT_FAILURE);
 	}
 	name = ft_strjoin("MiniRT - ", file);
 	if (!name)
-		exit(1);  // TODO
+		exit(EXIT_FAILURE);
 	vars.win = mlx_new_window(vars.mlx, scene->render->width,
 			scene->render->height, name);
+	if (!vars.win)
+	{
+		free_scene(scene);
+		perror("Error\nCan't generate the frame");
+		exit(EXIT_FAILURE);
+	}
 	vars.init_image = (t_bifun)mlx_init_image;
 	vars.set_pixel = (t_pixel_writer)mlx_set_pixel;
 	vars.on_refresh = (t_bicon)force_put_image;
@@ -46,7 +53,8 @@ void	init_window(char *file, t_scene *scene)
 	mlx_hook(vars.win, 17, 0L, &close_hook, &vars);
 	mlx_key_hook(vars.win, &key_hook, &vars);
 	mlx_string_put(vars.mlx, vars.win, 0, 50, ~0, "Press 'enter' to start");
-	mlx_string_put(vars.mlx, vars.win, 0, 70, ~0, "Press 'd' to start in debug mode");
+	mlx_string_put(vars.mlx, vars.win, 0, 70, ~0, "Press 'd' to start in debug \
+mode");
 	mlx_string_put(vars.mlx, vars.win, 0, 90, ~0, "Press 'esc' to exit");
 	mlx_loop(vars.mlx);
 }
