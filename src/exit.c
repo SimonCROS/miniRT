a3863d209926788c66d72d5e13d99798c8920fc4
Simon Cros <simon.dc.cros@gmail.com>
2021-03-27 00:41:06 +0100

    Save

--- src/exit.c
+++ src/exit.c
@@ -12,7 +12,7 @@ void	exit_minirt(t_vars *vars, t_tpool *pool, void *other, int __status)
 {
 	lst_foreachp(get_scene()->cameras, (t_bicon)free_camera_render, vars);
 	vars->on_exit(vars);
-	tpool_destroy(pool);
+	tpool_free(pool);
 	free_scene(get_scene());
 	free(other);
 	exit(__status);
