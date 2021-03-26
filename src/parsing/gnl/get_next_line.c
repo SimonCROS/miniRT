1ca1912f5a01f187bcb093a145f2ba4e8a5c8318
Simon Cros <simon.dc.cros@gmail.com>
2021-03-27 00:54:30 +0100

    Patch leaks in case of malloc fail in gnl (ft_split_first)

--- src/parsing/gnl/get_next_line.c
+++ src/parsing/gnl/get_next_line.c
@@ -1,6 +1,7 @@
 #include "minirt.h"
 
-static int	delete(int ret, t_gnl_entry **remain, t_gnl_entry *element, void *p)
+static int	free_gnl(int ret, t_gnl_entry **remain, t_gnl_entry *element,
+	void *p)
 {
 	t_gnl_entry	*elem;
 
@@ -71,7 +72,7 @@ static ssize_t	read_line(int fd, char **line, char ***current,
 	free(*current);
 	remain->content[result] = 0;
 	*current = ft_split_first(remain->content, '\n');
-	if (!current)
+	if (!*current)
 		return (-1);
 	if (result)
 	{
@@ -109,19 +110,19 @@ int	get_next_line(int fd, char **line)
 	if (!gnl_init(&current, &tmp_line, &result))
 		return (-1);
 	if (!get_or_create_remain(&remain_lst, fd, &remain))
-		return (delete(-1, NULL, NULL, tmp_line));
+		return (free_gnl(-1, NULL, NULL, tmp_line));
 	if (load_remain(remain, &tmp_line, &current) == -1)
-		return (delete(-1, &remain_lst, remain, tmp_line));
+		return (free_gnl(-1, &remain_lst, remain, tmp_line));
 	while (result == BUFF_SIZE && (!current || !current[1]))
 	{
 		result = read_line(fd, &tmp_line, &current, remain);
 		if (result == -1)
-			return (delete(-1, &remain_lst, remain, tmp_line));
+			return (free_gnl(-1, &remain_lst, remain, tmp_line));
 	}
 	remain->content = current[1];
 	free(current);
 	*line = tmp_line;
 	if (result == BUFF_SIZE || (result && remain->content))
 		return (1);
-	return (delete(0, &remain_lst, remain, NULL));
+	return (free_gnl(0, &remain_lst, remain, NULL));
 }
