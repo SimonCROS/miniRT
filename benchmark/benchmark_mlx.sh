a89ab2a6dbc6ba3d5cc4c75b32fedb31f654bd5a
Simon Cros <scros@student.42lyon.fr>
2021-03-27 16:48:29 +0100

    No leaks on benchmarks mlx and bmp

---
+++ benchmark/benchmark_mlx.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd $(dirname "$0")/..
+TIMEFORMAT="Time : %Rs"
+for entry in "scenes"/lite/*.rt "scenes"/crash/*.rt
+do
+	printf "\033[1;94mTesting %s...\033[0m\n" $entry
+	if command -v valgrind &> /dev/null
+	then
+		valgrind --tool=memcheck --leak-check=full --leak-resolution=high --show-reachable=yes ./miniRT $entry
+		printf "\n"
+	elif command -v leaks &> /dev/null
+	then
+		leaks -quiet -atExit -- ./miniRT $entry
+	fi
+done
