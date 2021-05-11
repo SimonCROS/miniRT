acdc8d1977348bde99a740f01899e58ca9dab1a6
Simon Cros <scros@student.42lyon.fr>
2021-05-11 17:54:37 +0200

    Start adding sepia function

---
+++ src/util/utils.c
@@ -0,0 +1,21 @@
+#include "minirt.h"
+
+void	get_samples_template(int aa, void *buf)
+{
+	if (aa == 1)
+		ft_memcpy(buf, (t_noaa){{0, 0}}, sizeof(t_noaa));
+	else if (aa == 4)
+		ft_memcpy(buf, (t_ssaa4){{0.25, 0.4}, {0.4, -0.25}, {-0.25, -0.4},
+		{-0.4, 0.25}}, sizeof(t_ssaa4));
+}
+
+t_color	to_sepia(t_color in)
+{
+	t_color	out;
+
+	out.r = (in.r * .393) + (in.g *.769) + (in.b * .189);
+	out.g = (in.r * .349) + (in.g *.686) + (in.b * .168);
+	out.b = (in.r * .272) + (in.g *.534) + (in.b * .131);
+	out.a = 1;
+	return (out);
+}
